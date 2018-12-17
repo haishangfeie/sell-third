f352dd396107b88b0c30b6e7dcd6ca308a809704
wenxingjie <wen_xingjie@163.com>
2018-12-17 19:02:48 +0800

    修改引入reset样式的方式

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
-import 'common/reset/reset.css'
+import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
